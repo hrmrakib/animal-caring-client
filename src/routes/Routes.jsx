96b8f6f99d1976923eb25956b3e2baec5512908a
hrmrakib <hrmrakibs@gmail.com>
2024-05-31 21:51:26 +0600

    navbar include

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../layouts/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <h2 className='text-3xl text-center font-bold'>404</h2>,
   },
 ]);
 
