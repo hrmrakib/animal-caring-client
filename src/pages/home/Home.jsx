84f6a7b751082234e93790067de637ae83dbd71c
hrmrakib <hrmrakibs@gmail.com>
2024-08-21 18:00:00 +0600

    remove: unused component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import HeroBanner from "../../components/header/HeroBanner";
 import PetsCategory from "./home/PetsCategory";
 import CallToAction from "./home/CallToAction";
-import AboutUs from "./home/AboutUs";
 import Overview from "./home/Overview";
 import WhyChooseUs from "./home/WhyChooseUs";
 import Testimonial from "./home/Testimonial";
@@ -26,7 +25,6 @@ const Home = () => {
       <div className='w-[90%] mx-auto'>
         <PetsCategory />
         <CallToAction />
-        {/* <AboutUs /> */}
       </div>
 
       <Overview />
